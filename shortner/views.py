aa395e01dbc136b346da88708bf10a0d2976e9a1
Shivang Garg <shivang10garg@gmail.com>
2022-01-21 22:36:52 +0530

    adds shorten url method

--- shortner/views.py
+++ shortner/views.py
@@ -1,6 +1,19 @@
+import uuid
+
+from django.http import HttpResponse
 from django.shortcuts import render
 
+from .models import Url
 
-# Create your views here.
+
 def index(request):
     return render(request, 'index.html')
+
+
+def create(request):
+    if request.method == 'POST':
+        link = request.POST['link']
+        uid = str(uuid.uuid4())[:5]
+        new_url = Url(link=link, uuid=uid)
+        new_url.save()
+        return HttpResponse(uid)
