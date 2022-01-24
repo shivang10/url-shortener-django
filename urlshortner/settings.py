b104b49b3dcb45bc6aa8107d2a3a37d09e5e65e7
Shivang Garg <shivang10garg@gmail.com>
2022-01-24 19:57:52 +0530

    adds dockerfile

--- urlshortner/settings.py
+++ urlshortner/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-(@7bp@te-a)xs-#231=u)#v9^n!0%xk1#!01(cpfndkxgiv%yj
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["0.0.0.0", "localhost"]
 
 
 # Application definition
